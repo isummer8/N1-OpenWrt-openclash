1da2473f8dfea25df66c031c3111a5d659ae5d01
AJ <983097239@qq.com>
2024-01-27 23:59:55 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,3 +1,2 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git openwrt-packages https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
